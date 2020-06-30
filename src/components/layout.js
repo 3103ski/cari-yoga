6d31359e7b3c10f8124daec423228b0d920ceb4a
3103ski <bjastski@gmail.com>
2020-06-30 16:30:00 -0700

    swaps meta thumb

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-import thumbImage from '../images/chakra-2.png'
+import thumbImage from '../images/door-yoga.jpg'
 
 import '../assets/scss/main.scss'
 
