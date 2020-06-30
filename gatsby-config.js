e28f64354fed0f7b44b5595866559f8f3de7bfad
3103ski <bjastski@gmail.com>
2020-06-30 16:53:05 -0700

    meta test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,24 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Yoga With Cari',
-    author: 'Bryan J',
-    description: 'A landing page for Cari Woodfields yoga services',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/chakra-2.png', // This path is relative to the root of the site.
-      },
+    siteMetadata: {
+        title: 'Yoga With Cari',
+        author: 'Bryan J',
+        description: 'A landing page for Cari Woodfields yoga services',
+        image: '/src/images/floor-split.jpg',
     },
-    'gatsby-plugin-sass',
-  ],
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: 'gatsby-starter-default',
+                short_name: 'starter',
+                start_url: '/',
+                background_color: '#663399',
+                theme_color: '#663399',
+                display: 'minimal-ui',
+                icon: 'src/images/chakra-2.png', // This path is relative to the root of the site.
+            },
+        },
+        'gatsby-plugin-sass',
+    ],
 }
