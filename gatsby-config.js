aab2168a04437c22c376db3e95ab64f9c3d10862
3103ski <bjastski@gmail.com>
2020-06-30 19:55:46 -0700

    checking safari meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
         title: 'Yoga With Cari',
         author: 'Bryan Jastrzembski',
         description: 'Do yoga with Cari!',
-        // image: '/src/images/floor-split.jpg',
+        image: '/src/images/floor-split.jpg',
     },
     plugins: [
         'gatsby-plugin-react-helmet',
@@ -18,7 +18,7 @@ module.exports = {
                 background_color: '#FF5733',
                 theme_color: '#663399',
                 display: 'minimal-ui',
-                image: 'src/images/door-yoga.jpg', // This path is relative to the root of the site.
+                image: 'src/images/grace.jpg', // This path is relative to the root of the site.
             },
         },
         'gatsby-plugin-sass',
