3236c88069483af54cf7c9d1a5a3dc0ebd537378
3103ski <bjastski@gmail.com>
2020-07-08 18:41:29 -0700

    eye rolling meta test

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-import thumbImage from '../images/grace.jpg'
+
+import thumbImage from '../images/grace.jpg?'
+
 import '../assets/scss/main.scss'
 
 const Layout = ({ children, location }) => {
